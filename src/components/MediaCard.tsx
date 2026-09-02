a46fd43e541fbc296950de10772aaab821217494
Jean-Simon Cyr <jsimcyr@gmail.com>
2026-09-02 12:00:46 -0400

    feat: add MediaRating component and integrate it into MediaCard for user ratings

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -1,12 +1,16 @@
 "use client"
 
 import {Media} from "@/src/lib/types";
+import {useState} from "react";
+import {MediaRating} from "@/src/components/MediaRating";
 
 type Props = {
     media: Media;
 }
 
 export function MediaCard({media}: Props) {
+
+    const [rating, setRating] = useState(0);
 
     return (
         <div className="card card-side bg-base-100 shadow-sm">
@@ -18,9 +22,21 @@ export function MediaCard({media}: Props) {
             <div className="card-body">
                 <h2 className="card-title">{media.title}</h2>
 
-                {media.genre.map((g) => (
-                    <div className="badge badge-info" key={g}>
-                        {g}
+                <div className={"flex flex-col gap-2"}>
+                    <span className={"text-lg font-bold"}>
+                        Moyenne: {media.avgScore?.toFixed(1) ?? "-"}
+                    </span>
+                </div>
+
+                <MediaRating
+                    mediaId={media.id}
+                    value={rating}
+                    onRatingChange={setRating}
+                />
+
+                {media.genre.map((genre) => (
+                    <div className="badge badge-info" key={genre}>
+                        {genre}
                     </div>
                 ))}
             </div>
